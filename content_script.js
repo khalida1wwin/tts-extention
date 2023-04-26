f3cf0bc8f4a048f6dcf7e4b685a13ae76cf0e732
khalida1wwin <77629619+khalida1wwin@users.noreply.github.com>
2023-04-26 12:16:04 -0600

    the voice playing at eh end is clear, but the downlead option is not

--- content_script.js
+++ content_script.js
@@ -1,31 +1,26 @@
 let speakerIcon = null;
 let playButton = null;
 let audio = null;
+let voiceSelection = null;
 
-function createPlayButton() {
-    if (!playButton) {
-        playButton = document.createElement('button');
-        playButton.innerHTML = 'Play TTS';
-        playButton.style.position = 'fixed';
-        playButton.style.zIndex = 10000;
-        playButton.style.right = '10px';
-        playButton.style.bottom = '10px';
-        document.body.appendChild(playButton);
-        playButton.addEventListener('click', () => {
-            if (audio) {
-                audio.play();
-            }
-        }, true);
-    }
-}
+let downloadButton = null;
 
-function playAudio() {
-  if (audio) {
-      audio.play();
-  } else {
-      setTimeout(playAudio, 100);
+function createDownloadButton(blob) {
+  console.log('Blob:', blob); // Add this line
+  if (!downloadButton) {
+    downloadButton = document.createElement('a');
+    downloadButton.innerHTML = 'Download TTS';
+    downloadButton.download = 'tts_audio.wav';
+    downloadButton.style.position = 'fixed';
+    downloadButton.style.zIndex = 10000;
+    downloadButton.style.right = '10px';
+    downloadButton.style.bottom = '50px';
+    document.body.appendChild(downloadButton);
   }
+  const url = URL.createObjectURL(blob);
+  downloadButton.href = url;
 }
+
 
 function showSpeakerIcon(x, y) {
   if (!speakerIcon) {
@@ -66,11 +61,16 @@ async function playNextChunk(audioList) {
 async function speakHighlightedText(e) {
   e.stopPropagation();
   const fullText = window.getSelection().toString();
+  // Get the selected voice from the storage
+  chrome.storage.local.get(['selectedVoice'], async (result) => {
+  const selectedVoice = result.selectedVoice || 'Unconditional';
+  console.log("Selected voice:", selectedVoice);
   
   if (fullText.length > 0) {
       // Split the text into 30-word chunks
       const words = fullText.split(' ');
       const chunks = [];
+      const audioCtx = new AudioContext();
       for (let i = 0; i < words.length; i += 30) {
           chunks.push(words.slice(i, i + 30).join(' '));
       }
@@ -78,9 +78,15 @@ async function speakHighlightedText(e) {
 
       // Fetch the audio for each chunk and store them in an array
       const audioList = [];
+      const audioBuffers = [];
       for (const chunk of chunks) {
           const formData = new FormData();
           formData.append('text', chunk);
+          if (selectedVoice) {
+            formData.append('history_prompt', selectedVoice);
+          }else{
+            formData.append('history_prompt', 'Unconditional');
+          }
           formData.append('history_prompt', 'Speaker 1 (en)'); // Replace with your desired prompt
 
           const requestOptions = {
@@ -90,7 +96,7 @@ async function speakHighlightedText(e) {
           };
           console.log("API calling", chunks);
           try {
-              const response = await fetch('https://deb5-35-247-169-249.ngrok.io/synthesize', requestOptions);
+              const response = await fetch('https://ed37-34-90-70-61.ngrok.io/synthesize', requestOptions);
               const data = await response.json();
               const audioData = atob(data.audio_data);
               const audioBytes = new Uint8Array(audioData.length);
@@ -98,21 +104,82 @@ async function speakHighlightedText(e) {
                   audioBytes[i] = audioData.charCodeAt(i);
               }
 
-              const audioCtx = new AudioContext();
+              
               const blob = new Blob([audioBytes.buffer], { type: "audio/wav" });
               const blobUrl = URL.createObjectURL(blob);
               audioList.push(blobUrl);
+              // Decode the audio data and store the resulting AudioBuffer
+              const decodedAudio = await audioCtx.decodeAudioData(audioBytes.buffer);
+              audioBuffers.push(decodedAudio);
           } catch (error) {
               console.error('Error fetching synthesized speech:', error);
           }
       }
+      const fullBuffer = concatAudioBuffers(audioCtx, audioBuffers);
+      // Convert the full AudioBuffer to a Blob and create the Download button
+      const fullBlob = await (await audioBufferToWaveBlob(fullBuffer));
+
+
+      createDownloadButton(fullBlob);
+
+      console.log("Fetched audio for all chunks");
 
       console.log("Fetched audio for all chunks");
       playNextChunk(audioList);
   }
+  });
+}
+async function audioBufferToWaveBlob(audioBuffer) {
+  const numberOfChannels = audioBuffer.numberOfChannels;
+  const lengthInSamples = audioBuffer.length;
+  const sampleRate = audioBuffer.sampleRate;
+
+  const waveHeader = new ArrayBuffer(44);
+  const waveData = new Float32Array(lengthInSamples * numberOfChannels);
+
+  for (let channelIndex = 0; channelIndex < numberOfChannels; channelIndex++) {
+    const channelData = audioBuffer.getChannelData(channelIndex);
+
+    for (let i = 0; i < lengthInSamples; i++) {
+      waveData[i * numberOfChannels + channelIndex] = channelData[i];
+    }
+  }
+
+  const waveDataView = new DataView(waveHeader);
+
+  waveDataView.setUint32(0, 0x52494646, false); // "RIFF"
+  waveDataView.setUint32(4, 36 + waveData.byteLength, true);
+  waveDataView.setUint32(8, 0x57415645, false); // "WAVE"
+  waveDataView.setUint32(12, 0x666d7420, false); // "fmt "
+  waveDataView.setUint32(16, 16, true); // Subchunk1Size (16 for PCM)
+  waveDataView.setUint16(20, 1, true); // AudioFormat (1 for PCM)
+  waveDataView.setUint16(22, numberOfChannels, true);
+  waveDataView.setUint32(24, sampleRate, true);
+  waveDataView.setUint32(28, sampleRate * numberOfChannels * 2, true); // ByteRate
+  waveDataView.setUint16(32, numberOfChannels * 2, true); // BlockAlign
+  waveDataView.setUint16(34, 16, true); // BitsPerSample
+  waveDataView.setUint32(36, 0x64617461, false); // "data"
+  waveDataView.setUint32(40, waveData.byteLength, true);
+
+  return new Blob([waveHeader, waveData], { type: "audio/wav" });
 }
 
+// Function to concatenate multiple AudioBuffers
+function concatAudioBuffers(audioCtx, buffers) {
+  const numberOfChannels = buffers[0].numberOfChannels;
+  const totalLength = buffers.reduce((acc, buf) => acc + buf.length, 0);
+  const result = audioCtx.createBuffer(numberOfChannels, totalLength, buffers[0].sampleRate);
 
+  for (let channelIndex = 0; channelIndex < numberOfChannels; channelIndex++) {
+    let offset = 0;
+    for (const buffer of buffers) {
+      result.copyToChannel(buffer.getChannelData(channelIndex), channelIndex, offset);
+      offset += buffer.length;
+    }
+  }
+
+  return result;
+}
 
 document.addEventListener('mouseup', (e) => {
   const selectedText = window.getSelection().toString();
@@ -126,3 +193,4 @@ document.addEventListener('mouseup', (e) => {
 document.addEventListener('mousedown', () => {
   hideSpeakerIcon();
 });
+
--- popup.js
+++ popup.js
@@ -1,30 +1,36 @@
-const voiceSelect = document.getElementById('voiceSelect');
 
-function loadVoices() {
-  const voices = speechSynthesis.getVoices();
-  voices.forEach((voice) => {
+
+/////////////
+
+const AVAILABLE_PROMPTS = ['Unconditional', 'Announcer', 'Speaker 0 (en)', 'Speaker 1 (en)', 'Speaker 2 (en)', 'Speaker 3 (en)', 'Speaker 4 (en)', 'Speaker 5 (en)', 'Speaker 6 (en)', 'Speaker 7 (en)', 'Speaker 8 (en)', 'Speaker 9 (en)', 'Speaker 0 (de)', 'Speaker 1 (de)', 'Speaker 2 (de)', 'Speaker 3 (de)', 'Speaker 4 (de)', 'Speaker 5 (de)', 'Speaker 6 (de)', 'Speaker 7 (de)', 'Speaker 8 (de)', 'Speaker 9 (de)', 'Speaker 0 (es)', 'Speaker 1 (es)', 'Speaker 2 (es)', 'Speaker 3 (es)', 'Speaker 4 (es)', 'Speaker 5 (es)', 'Speaker 6 (es)', 'Speaker 7 (es)', 'Speaker 8 (es)', 'Speaker 9 (es)', 'Speaker 0 (fr)', 'Speaker 1 (fr)', 'Speaker 2 (fr)', 'Speaker 3 (fr)', 'Speaker 4 (fr)', 'Speaker 5 (fr)', 'Speaker 6 (fr)', 'Speaker 7 (fr)', 'Speaker 8 (fr)', 'Speaker 9 (fr)', 'Speaker 0 (hi)', 'Speaker 1 (hi)', 'Speaker 2 (hi)', 'Speaker 3 (hi)', 'Speaker 4 (hi)', 'Speaker 5 (hi)', 'Speaker 6 (hi)', 'Speaker 7 (hi)', 'Speaker 8 (hi)', 'Speaker 9 (hi)', 'Speaker 0 (it)', 'Speaker 1 (it)', 'Speaker 2 (it)', 'Speaker 3 (it)', 'Speaker 4 (it)', 'Speaker 5 (it)', 'Speaker 6 (it)', 'Speaker 7 (it)', 'Speaker 8 (it)', 'Speaker 9 (it)', 'Speaker 0 (ja)', 'Speaker 1 (ja)', 'Speaker 2 (ja)', 'Speaker 3 (ja)', 'Speaker 4 (ja)', 'Speaker 5 (ja)', 'Speaker 6 (ja)', 'Speaker 7 (ja)', 'Speaker 8 (ja)', 'Speaker 9 (ja)', 'Speaker 0 (ko)', 'Speaker 1 (ko)', 'Speaker 2 (ko)', 'Speaker 3 (ko)', 'Speaker 4 (ko)', 'Speaker 5 (ko)', 'Speaker 6 (ko)', 'Speaker 7 (ko)', 'Speaker 8 (ko)', 'Speaker 9 (ko)', 'Speaker 0 (pl)', 'Speaker 1 (pl)', 'Speaker 2 (pl)', 'Speaker 3 (pl)', 'Speaker 4 (pl)', 'Speaker 5 (pl)', 'Speaker 6 (pl)', 'Speaker 7 (pl)', 'Speaker 8 (pl)', 'Speaker 9 (pl)', 'Speaker 0 (pt)', 'Speaker 1 (pt)', 'Speaker 2 (pt)', 'Speaker 3 (pt)', 'Speaker 4 (pt)', 'Speaker 5 (pt)', 'Speaker 6 (pt)', 'Speaker 7 (pt)', 'Speaker 8 (pt)', 'Speaker 9 (pt)', 'Speaker 0 (ru)', 'Speaker 1 (ru)', 'Speaker 2 (ru)', 'Speaker 3 (ru)', 'Speaker 4 (ru)', 'Speaker 5 (ru)', 'Speaker 6 (ru)', 'Speaker 7 (ru)', 'Speaker 8 (ru)', 'Speaker 9 (ru)', 'Speaker 0 (tr)', 'Speaker 1 (tr)', 'Speaker 2 (tr)', 'Speaker 3 (tr)', 'Speaker 4 (tr)', 'Speaker 5 (tr)', 'Speaker 6 (tr)', 'Speaker 7 (tr)', 'Speaker 8 (tr)', 'Speaker 9 (tr)', 'Speaker 0 (zh)', 'Speaker 1 (zh)', 'Speaker 2 (zh)', 'Speaker 3 (zh)', 'Speaker 4 (zh)', 'Speaker 5 (zh)', 'Speaker 6 (zh)', 'Speaker 7 (zh)', 'Speaker 8 (zh)', 'Speaker 9 (zh)'];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const voiceSelection = document.getElementById('voiceSelection');
+  populateVoiceDropdown(voiceSelection);
+});
+
+function populateVoiceDropdown(selectElement) {
+  AVAILABLE_PROMPTS.forEach(prompt => {
     const option = document.createElement('option');
-    option.textContent = voice.name;
-    option.value = voice.name;
-    voiceSelect.appendChild(option);
+    option.value = prompt;
+    option.text = prompt;
+    selectElement.add(option);
   });
 }
 
-function saveVoiceSelection() {
-  const selectedVoice = voiceSelect.value;
-  chrome.storage.sync.set({ selectedVoice: selectedVoice });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const voiceSelection = document.getElementById('voiceSelection');
+  populateVoiceDropdown(voiceSelection);
 
-// Load voices when speechSynthesis is updated or the popup is opened
-speechSynthesis.addEventListener('voiceschanged', loadVoices);
-document.addEventListener('DOMContentLoaded', loadVoices);
+  // Load the currently selected voice from the storage
+  chrome.storage.local.get(['selectedVoice'], (result) => {
+    if (result.selectedVoice) {
+      voiceSelection.value = result.selectedVoice;
+    }
+  });
 
-// Save the user's voice selection
-voiceSelect.addEventListener('change', saveVoiceSelection);
-
-// Load the user's saved voice selection
-chrome.storage.sync.get('selectedVoice', (data) => {
-  if (data.selectedVoice) {
-    voiceSelect.value = data.selectedVoice;
-  }
+  // Save the selected voice to the storage
+  voiceSelection.addEventListener('change', () => {
+    chrome.storage.local.set({ selectedVoice: voiceSelection.value });
+  });
 });
